730233ea06a5649748587fc03a8f19f4608f5630
LucasLyons <120273381+LucasLyons@users.noreply.github.com>
2025-09-28 20:32:39 -0400

    auth fixes + added supabase SSR

--- brewtiful-web/app/beers/page.tsx
+++ brewtiful-web/app/beers/page.tsx
@@ -1,10 +1,6 @@
 import { createClient } from '@/utils/supabase/server';
-
 export default async function Beers() {
   const supabase = await createClient();
   const { data: beers } = await supabase.from("beers").select();
-  console.log('URL:', process.env.NEXT_PUBLIC_SUPABASE_URL)
-  console.log('Key:', process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY)
-
   return <pre>{JSON.stringify(beers, null, 2)}</pre>
 }
