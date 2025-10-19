bd1912af82050156543c717b0ebc6e79d5ebd2a5
LucasLyons <120273381+LucasLyons@users.noreply.github.com>
2025-10-19 11:41:25 -0400

    fix

--- brewtiful-web/app/beers/page.tsx
+++ brewtiful-web/app/beers/page.tsx
@@ -3,5 +3,5 @@ import { createClient } from '@/utils/supabase/server';
 export default async function Page() {
   const supabase = await createClient();
   const { data: beers } = await supabase.from("beers").select();
-  return <main>{JSON.stringify(beers, null, 2)}</main>
+  return <pre>{JSON.stringify(beers, null, 2)}</pre>
 }
